3b4b8932c17bc1ea409d2671555aeedcc9568adc
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-09 20:23:41 +0530

    feat: setup AuthLayout and HostLayout

--- client/src/components/Layouts/AuthLayout.tsx
+++ client/src/components/Layouts/AuthLayout.tsx
@@ -1,7 +1,10 @@
-const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+import { Navigate, Outlet, useLocation } from "react-router";
 
-export default AuthLayout
+const AuthLayout = () => {
+	const pathname = useLocation().state?.redirectTo || "/Home";
+	const user = false;
+
+	return user ? <Navigate to={pathname} replace /> : <Outlet />;
+};
+
+export default AuthLayout;
--- client/src/components/Layouts/HostLayout.tsx
+++ client/src/components/Layouts/HostLayout.tsx
@@ -1,7 +1,18 @@
-const HostLayout = () => {
-  return (
-    <div>HostLayout</div>
-  )
-}
+import { Navigate, Outlet, useLocation } from "react-router";
 
-export default HostLayout
+const HostLayout = () => {
+	const pathname = useLocation().pathname;
+	const user = true;
+
+	return user ? (
+		<Outlet />
+	) : (
+		<Navigate
+			to="/login"
+			state={{ message: "You Must Login First", redirectTo: pathname }}
+			replace
+		/>
+	);
+};
+
+export default HostLayout;
