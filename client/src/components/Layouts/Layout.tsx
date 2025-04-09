427989ed0e3e1ce46a1ef5cd449431a1b5b7bf2b
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-09 17:02:16 +0530

    feat: Updated NotFound page

--- client/src/components/Layouts/Layout.tsx
+++ client/src/components/Layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
 
-const Layout = () => <Outlet />
+const Layout = () => <main><Outlet /></main>
 
 export default Layout
--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router";
 
 const NotFound = () => (
-	<main className="min-h-screen flex flex-col justify-center items-center gap-10 text-center">
-		<h1 className="text-gray-600 text-2xl font-bold sm:text-4xl">404 - PAGE NOT FOUND</h1>
+	<main className="min-h-screen flex flex-col justify-center items-center gap-8 text-center">
+		<h1 className="text-gray-600 text-2xl font-secondary font-bold sm:text-4xl">404 - PAGE NOT FOUND!</h1>
 		<NavLink
-			to="home"
+			to="/home"
 			replace
 			className="btn"
 		>
