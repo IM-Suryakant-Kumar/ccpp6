2ac02cd36dd05b3d0addc03e5c241e6b1e7c86b8
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-09 17:54:33 +0530

    feat: Added Landing page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,16 +1,17 @@
-import { NavLink } from "react-router";
+import { Link } from "react-router";
 
 const NotFound = () => (
-	<main className="min-h-screen flex flex-col justify-center items-center gap-8 text-center">
+	<article className="min-h-screen flex flex-col justify-center items-center gap-8 text-center">
 		<h1 className="text-gray-600 text-2xl font-secondary font-bold sm:text-4xl">404 - PAGE NOT FOUND!</h1>
-		<NavLink
+    {/* redirect to home */}
+		<Link
 			to="/home"
 			replace
 			className="btn"
 		>
 			Go back home
-		</NavLink>
-	</main>
+		</Link>
+	</article>
 );
 
 export default NotFound;
