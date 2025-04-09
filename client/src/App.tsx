ad890b39dd968ca672b89c2b199dccb8c8416a60
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-09 16:26:57 +0530

    feat: Added NotFound page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,44 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router";
+import { AuthLayout, HostLayout, Layout } from "./components";
+import {
+	Explore,
+	Home,
+	Landing,
+	LikedPosts,
+	Login,
+	NotFound,
+	Posts,
+	Profile,
+	SavedPosts,
+	Setting,
+	Signup,
+} from "./pages";
+
 const App = () => {
-	return (
-		<>
-			<h1>hello</h1>
-		</>
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Landing />} />
+				<Route element={<HostLayout />}>
+					<Route path="home" element={<Home />} />
+					<Route path="explore" element={<Explore />} />
+					<Route path="profile" element={<Profile />}>
+						<Route index element={<Posts />} />
+						<Route path="liked" element={<LikedPosts />} />
+						<Route path="saved" element={<SavedPosts />} />
+					</Route>
+					<Route path="settings" element={<Setting />} />
+				</Route>
+				<Route element={<AuthLayout />}>
+					<Route path="login" element={<Login />} />
+					<Route path="signup" element={<Signup />} />
+				</Route>
+				<Route path="*" element={<NotFound />} />
+			</Route>
+		)
 	);
+
+	return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router";
+
+const NotFound = () => (
+	<main className="min-h-screen flex flex-col justify-center items-center gap-10 text-center">
+		<h1 className="text-gray-600 text-2xl font-bold sm:text-4xl">404 - PAGE NOT FOUND</h1>
+		<NavLink
+			to="home"
+			replace
+			className="btn"
+		>
+			Go back home
+		</NavLink>
+	</main>
+);
+
+export default NotFound;
