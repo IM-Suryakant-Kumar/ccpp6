639fc6cae56d9cb3497850f08864f7e23c3509ae
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-21 11:53:06 +0530

    feat: Added Filters

--- client/src/pages/Host/Home.tsx
+++ client/src/pages/Host/Home.tsx
@@ -1,13 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Home = () => {
-  const [filter, setFilter] = useState<string>("recent")
+	const [filter, setFilter] = useState<string>("recent");
 
-  return (
-    <article className="max-w-xl mx-auto">
-      home
-    </article>
-  )
-}
+  const handleFilter = (newFilter: string) => {
+    if(newFilter !== filter) setFilter(newFilter)
+  }
 
-export default Home
+	return (
+		<article className="max-w-xl mx-auto">
+			{/* filters */}
+			<section className="flex justify-between items-center gap-4 mx-2 py-4 border-b-2 border-blue-200">
+				<button className={`filters-btn ${filter === "recent" && "bg-logo text-primary"}`} onClick={() => handleFilter("recent")}>
+					Recent
+				</button>
+				<button className={`filters-btn ${filter === "older" && "bg-logo text-primary"}`} onClick={() => handleFilter("older")}>
+					Older
+				</button>
+				<button className={`filters-btn ${filter === "trending" && "bg-logo text-primary"}`} onClick={() => handleFilter("trending")}>
+					Trending
+				</button>
+			</section>
+			home
+		</article>
+	);
+};
+
+export default Home;
